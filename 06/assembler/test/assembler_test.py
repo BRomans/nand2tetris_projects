35a4bbed7bc53db0b60efd5a1d693f5091344986
BRomans <michele.romani.gzl0@gmail.com>
2019-08-11 14:03:59 +0200

    :rocket: Setup unittest for python3

--- 06/assembler/test/assembler_test.py
+++ 06/assembler/test/assembler_test.py
@@ -1,21 +1,27 @@
 from src.parser import Parser
 from src.code import Code
 from src.symbol_table import SymbolTable
+import unittest
 
 
-class TestParser:
+class TestParser(unittest.TestCase):
 
     def test_has_more_commands(self):
         print("todo")
 
 
-class TestCode:
+class TestCode(unittest.TestCase):
 
     def test_dest(self):
         print("todo")
+    
+    def test_comp(self):
+        print("todo")
+        
+    def test_jump(self):
+        print("todo")
 
-
-class TestSymbolTable:
+class TestSymbolTable(unittest.TestCase):
 
     def test_add_entry(self):
         print("todo")
